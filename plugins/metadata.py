2e6823b0f6869488c8cdbdd231b4e4a9c1ed2d40
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2024-05-12 19:26:38 +0530

    Update metadata.py

--- plugins/metadata.py
+++ plugins/metadata.py
@@ -1,7 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from helper.database import db
-from pyromod.exceptions import ListenerTimeout
 from config import Txt
 
 
@@ -42,8 +41,8 @@ async def query_metadata(bot: Client, query: CallbackQuery):
         await query.message.delete()
         try:
             try:
-                metadata = await bot.ask(text=Txt.SEND_METADATA, chat_id=query.from_user.id, filters=filters.text, timeout=30, disable_web_page_preview=True)
-            except ListenerTimeout:
+                metadata = await bot.ask(text=Txt.SEND_METADATA, chat_id=query.from_user.id, filters=filters.text, disable_web_page_preview=True)
+            except:
                 await query.message.reply_text("⚠️ Error!!\n\n**Request timed out.**\nRestart by using /metadata", reply_to_message_id=query.message.id)
                 return
             print(metadata.text)
