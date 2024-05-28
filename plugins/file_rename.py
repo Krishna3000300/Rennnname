58a072ecc14ec41b8b41238778489c5af0fc1c83
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2024-05-28 22:02:46 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -19,7 +19,7 @@ from typing import Tuple
 
 LOG_CHANNEL = Config.LOG_CHANNEL
 
-@Client.on_message(filters.text & filters.private & ~filters.command(['start','help','users','broadcast','batch','genlink','stats']))
+@Client.on_message(filters.text & filters.private & ~filters.command(['start','help','about','showthumbnail','deletethumbnail','set_caption','get_caption','del_caption','change_mode','get_mode','stats','restart','broadcast',]))
 def send_text(client, message):
     user = message.text
     first_60_letters = user[:60]
